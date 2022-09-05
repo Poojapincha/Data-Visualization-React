958b209a86f8bb3c25a7feb76f921dee37390bcc
pooja pincha <poojapincha88@gmail.com>
2022-09-05 16:53:14 +0530

    added files

---
+++ intern/my-app1/src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import Barchart from './components/Barchart.js';
+import Sbar from './components/Sbar.js';
+function App() {
+  return (
+    <>
+      <Barchart /> 
+      <Sbar />
+
+    </>
+  );
+}
+
+export default App;
